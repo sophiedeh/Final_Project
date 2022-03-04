d68b0580e1cace76619f496e7c868004557c3e87
sophiedeh <100349141+sophiedeh@users.noreply.github.com>
2022-03-04 10:42:57 +0100

    New models with 3 hidden layers, witdth 200 and default initialization, batchsize 100

--- Train_network.py
+++ Train_network.py
@@ -9,7 +9,7 @@ from torch import nn
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import time
-from Create_5_models_w10d3 import NeuralNetwork
+from Create_5_models_w200d5 import NeuralNetwork
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -51,8 +51,8 @@ def test(dataloader, model, loss_fn):
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 training_times = 5 #amount of how many times to train data
-width = 10 #amount of nodes
-depth = 3 #amount of layers
+width = 200 #amount of nodes
+depth = 5 #amount of layers
 
 # Load training data from own script. 
 training_data = torch.load('mini_pca_train.pt')
@@ -64,7 +64,7 @@ test_data = torch.load('mini_pca_test.pt')
 dataset_size = len(training_data)
 
 # Define the batch size
-batch_size = 64
+batch_size = 100
 
 for u in range(training_times):       
     # Create data loaders.
@@ -81,7 +81,7 @@ for u in range(training_times):
     print(f"Using {device} device")
     
     model = NeuralNetwork(width=width)
-    model.load_state_dict(torch.load(f"initial_model_random_{u+1}_w{width}_d{depth}.pth"))    
+    model.load_state_dict(torch.load(f"initial_model_{u+1}_w{width}_d{depth}.pth"))    
     
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
@@ -113,14 +113,15 @@ for u in range(training_times):
 
     for ax in axs.flat:
         ax.set(xlabel='Number of steps', ylabel='Loss')
+        ax.yscale('log')
 
     for ax in axs.flat:
         ax.label_outer()
 
     plt.show()
-    fig.savefig(f"Plot_random_bs{batch_size}_w{width}_d{depth}_version{u+1}_e{epochs}_tt{training_times}.pdf")
+    fig.savefig(f"Plot_bs{batch_size}_w{width}_d{depth}_version{u+1}_e{epochs}_tt{training_times}.pdf")
     
-    torch.save(model.state_dict(), f"model_random_bs{batch_size}_w{width}_d{depth}_version{u+1}_e{epochs}_tt{training_times}.pth")
-    print(f"Saved PyTorch Model State to model_random_bs{batch_size}_w{width}_d{depth}_version{u+1}_e{epochs}_tt{training_times}.pth")
+    torch.save(model.state_dict(), f"model_bs{batch_size}_w{width}_d{depth}_version{u+1}_e{epochs}_tt{training_times}.pth")
+    print(f"Saved PyTorch Model State to model_bs{batch_size}_w{width}_d{depth}_version{u+1}_e{epochs}_tt{training_times}.pth")
     
     #batch_size += 50
