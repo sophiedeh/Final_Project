92d1916de87ee3218421291e131bb3f9cb5f635f
sophiedeh <100349141+sophiedeh@users.noreply.github.com>
2022-03-09 10:08:01 +0100

    Mse loss with orignal network

--- Create_5_models_w512d3.py
+++ Create_5_models_w512d3.py
@@ -31,5 +31,5 @@ numbers_of_models = 5
 
 for i in range(numbers_of_models): 
     model = NeuralNetwork(width=width)
-    torch.save(model.state_dict(), f"initial_model_0503_{i+1}_w{width}_d{depth}.pth")
-    print(f"Saved PyTorch Model State to initial_model_0503_{i+1}_w{width}_d{depth}.pth")
+    torch.save(model.state_dict(), f"initial_model_{i+1}_w{width}_d{depth}.pth")
+    print(f"Saved PyTorch Model State to initial_model_{i+1}_w{width}_d{depth}.pth")
