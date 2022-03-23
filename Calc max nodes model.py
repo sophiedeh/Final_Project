c2f99fcd5ca54fb79c5adc528360ebb863843c1d
sophiedeh <100349141+sophiedeh@users.noreply.github.com>
2022-03-23 15:14:38 +0100

    Code to calculate nodes

---
+++ Calc max nodes model.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 14:52:27 2022
+
+@author: 20182672
+"""
+
+def max_nodes_model(input, output, width, layers):
+    total = input*width + layers*width*width + width*output
+    return total
+
+max_nodes_model(10, 1, 10, 5)
