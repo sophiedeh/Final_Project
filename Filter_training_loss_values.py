2ac577c48204903cd83abc35eb18c7a3275057ab
sophiedeh <100349141+sophiedeh@users.noreply.github.com>
2022-04-10 14:39:12 +0200

    Cleaned only savgol

--- Filter_training_loss_values.py
+++ Filter_training_loss_values.py
@@ -6,8 +6,7 @@ Created on Thu Apr  7 14:22:52 2022
 """
 import torch
 import matplotlib.pyplot as plt
-from scipy.signal import savgol_filter, medfilt, sosfiltfilt, butter
-import pandas as pd
+from scipy.signal import savgol_filter
 
 batch_size = 200
 dataset_size = 1000
@@ -21,24 +20,17 @@ different_width = 1
 number_of_steps = []
 
 #  Load the training loss values and the derivatives from the trained networks.    
-#derivatives = torch.load(f"Derivatives_train_bs{batch_size}_w{width}_hl{hidlay}_ds{dataset_size}_e{epochs}_tt{training_times}.pt")
 loss_values_train = torch.load(f"Loss_values_train_bs{batch_size}_w{width}_hl{hidlay}_ds{dataset_size}_e{epochs}_tt{training_times}.pt")
 
+# Define number of steps array
 for i in range(len(loss_values_train)):
      number_of_steps.append((i+1)*(dataset_size/batch_size))
 
-df = pd.DataFrame(loss_values_train)
 # Smooth loss values and create tensor
-
-filter_times = 1000
-window = 51
-for i in range(filter_times):
-     #df[1] = df[0].rolling(window=window).mean()                  
-     #loss_values_train = savgol_filter(loss_values_train, window, 3)
-     # loss_values_train = medfilt(loss_values_train, kernel_size = window)
-     sos = butter(8, 0.125, output='sos')
-     loss_values_train = sosfiltfilt(sos,loss_values_train)
-loss_values_train = loss_values_train.copy()
+filter_times = 100
+window = 101
+for i in range(filter_times):              
+     loss_values_train = savgol_filter(loss_values_train, window, 3)
 loss_values_train_tensor = torch.Tensor(loss_values_train)
 
 fig, axs = plt.subplots(2,sharex=True)
