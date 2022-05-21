54dd109fdbb07b86df6e35ab62b66408d19bae9b
sophiedeh <100349141+sophiedeh@users.noreply.github.com>
2022-05-21 17:42:04 +0200

    Measurement of escape steps percentage

--- Models to measure/Models w100 hl10 v1-6/Escape steps percentage.py
+++ Models to measure/Models w100 hl10 v1-6/Escape steps percentage.py
@@ -18,42 +18,111 @@ version = 4
 for u in range(version):
     batch_size = 25
     plt.figure()
-    plt.title(f"Loss values of network with escape point width {width}, hidden layers {hidlay} and version {u+1}")
+    plt.title(f"Loss values of network with width {width}, hidden layers {hidlay} and version {u+1}")
+    escape_steps_values = []
+    escape_positions = []
+    batch_sizes = []
     for i in range(training_times):
-            number_of_steps = []
-            escape_steps = []
-
-            # Load loss values
-            loss_values_train = torch.load(f"Loss_values_train_bs{batch_size}_w{width}_hl{hidlay}_v{u+1}_ds{dataset_size}_e{epochs}_tt{training_times}.pt")
-
-            # for i in range(len(loss_values_train)):
-            #     number_of_steps.append((i+1)*(dataset_size/batch_size)) # i + 1 since first value corresponds to first epoch so not zero epoch
+        number_of_steps = []
+        
+        # Load loss values
+        loss_values_train = torch.load(f"Loss_values_train_bs{batch_size}_w{width}_hl{hidlay}_v{u+1}_ds{dataset_size}_e{epochs}_tt{training_times}.pt")
+        loss_values = []
+        
+        for i in range(0, 50000, 100):
+            loss_values.append(loss_values_train[i])
+            number_of_steps.append((i+1)*(dataset_size/batch_size))
+        # for i in range(len(loss_values_train)):
+        #     number_of_steps.append((i+1)*(dataset_size/batch_size))
+        
+        #fig = plt.figure()
+        #ax = fig.subplots()
+        	
+        escapes_for_value = []
+        for x in range(len(loss_values_train)):
+            escapes_for_value.append((x+1)*(dataset_size/batch_size))
                 
-            t = 1
-            while loss_values_train[t] >= 0.9*loss_values_train[0]:
-                t += 1
-            else:
-                escape_step_value = loss_values_train[t]
-            #escape_steps.append(escape_step_value)
-                
-            loss_values = []
-            for i in range(0, 50000, 100):
-                loss_values.append(loss_values_train[i])
-                number_of_steps.append((i+1)*(dataset_size/batch_size))
-            
-            plt.plot(number_of_steps, loss_values,'-')
-            plt.yscale('log')
-            #plt.legend(f"{batch_size}")
-            plt.xlabel("Number of steps (-)")
-            plt.ylabel("Loss value (-)")
-            plt.scatter(number_of_steps, escape_step_value)
-            plt.xlim([85000,140000])
-            plt.ylim(10**(-3),0)
-            
-            batch_size += 25
+        t = 1
+        while loss_values_train[t] >= 0.95*loss_values_train[0]:
+            t += 1
+            escape_step_value = loss_values_train[t+1]
+            escape_pos = escapes_for_value[t+1]
+        escape_steps_values.append(escape_step_value)
+        escape_positions.append(escape_pos)
+        
+        plt.plot(number_of_steps, loss_values,'-',escape_pos)
+        plt.yscale('log')
+        #plt.legend(f"{batch_size}")
+        plt.xlabel("Number of steps (-)")
+        plt.ylabel("Loss value (-)")
+        plt.scatter(escape_pos, escape_step_value)
+        plt.xlim([85000,140000])
+        plt.ylim(10**(-3),0)
+        
+        batch_sizes.append(batch_size)
+        batch_size += 25   
     plt.legend(['Bs 25','Bs 50', 'Bs 75','Bs 100', 'Bs 125', 'Bs 150', 'Bs 175', 'Bs 200','Bs 225', 'Bs 250', 'Bs 275', 'Bs 300'])
     plt.show()
-    plt.savefig(f"Loss values with escape steps of w{width} hl{hidlay} v{u+1}.pdf",bbox_inches='tight')
+    plt.savefig(f"All loss values of w{width} hl{hidlay} v{u+1}.pdf",bbox_inches='tight')
+    
+    plt.plot(batch_sizes, escape_steps_values,'-', marker='o')
+    plt.show()
+    plt.savefig(f"Escape steps percentage of w{width} hl{hidlay} v{u+1}.pdf",bbox_inches='tight')
+
+
+
+
+
+
+
+# for u in range(version):
+#     batch_size = 25
+#     plt.figure()
+#     plt.title(f"Loss values of network with escape point width {width}, hidden layers {hidlay} and version {u+1}")
+#     for i in range(training_times):
+#         number_of_steps = []
+
+#         # Load loss values
+#         loss_values_train = torch.load(f"Loss_values_train_bs{batch_size}_w{width}_hl{hidlay}_v{u+1}_ds{dataset_size}_e{epochs}_tt{training_times}.pt")
+
+#         # for i in range(len(loss_values_train)):
+#         #     number_of_steps.append((i+1)*(dataset_size/batch_size)) # i + 1 since first value corresponds to first epoch so not zero epoch
+        
+#         loss_values = []
+#         for i in range(0, 50000, 100):
+#             loss_values.append(loss_values_train[i])
+#             number_of_steps.append((i+1)*(dataset_size/batch_size))
+        
+#         escapes_for_value = []
+#         for x in range(len(loss_values_train)):
+#             escapes_for_value.append((x+1)*(dataset_size/batch_size))
+        
+#         t = 1
+#         while loss_values_train[t] >= 0.9*loss_values_train[0]:
+#             t += 1
+#         else:
+#             escape_step_value = loss_values_train[t]
+#             escape_pos = escapes_for_value[t]
+#             break
+            
+#         # loss_values = []
+#         # for i in range(0, 50000, 100):
+#         #     loss_values.append(loss_values_train[i])
+#         #     number_of_steps.append((i+1)*(dataset_size/batch_size))
+        
+#         plt.plot(number_of_steps, loss_values,'-')# escape_pos, escape_step_value, marker = 'o')
+#         plt.yscale('log')
+#         #plt.legend(f"{batch_size}")
+#         plt.xlabel("Number of steps (-)")
+#         plt.ylabel("Loss value (-)")
+#         #plt.scatter(escape_pos, escape_step_value)
+#         #plt.xlim([85000,140000])
+#         #plt.ylim(10**(-3),0)
+        
+#         batch_size += 25
+#     plt.legend(['Bs 25','Bs 50', 'Bs 75','Bs 100', 'Bs 125', 'Bs 150', 'Bs 175', 'Bs 200','Bs 225', 'Bs 250', 'Bs 275', 'Bs 300'])
+#     plt.show()
+#     plt.savefig(f"Loss values with escape steps of w{width} hl{hidlay} v{u+1}.pdf",bbox_inches='tight')
                 
                 
                 
